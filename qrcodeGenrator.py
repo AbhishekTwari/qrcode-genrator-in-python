6ceaf1d08122713b64045c02d360a18484dfb6be
AbhishekTwari <111764092+AbhishekTwari@users.noreply.github.com>
2022-12-20 20:15:34 +0530

    Update qrcodeGenrator.py

--- qrcodeGenrator.py
+++ qrcodeGenrator.py
@@ -1,3 +1,10 @@
-import qrcode as qr
-img = qr.make("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl")
-img.save("hello.png")
+import qrcode
+
+from PIL import Image
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_M,
+                   box_size=12,border=4)
+qr.add_data("https://github.com/AbhishekTwari")
+qr.make(fit=True)
+img = qr.make_image()
+img.save("qrImage.png")
